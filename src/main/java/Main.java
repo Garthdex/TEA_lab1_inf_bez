cb3a9e2bdcc13a8daf5308dfb7888c72ff907e00
Kuzmin Anton <Garthdex@gmail.com>
2017-03-09 15:40:40 +0400

    документация при аттрибуте "?"

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,6 @@
 import java.io.*;
 
-
 public class Main {
-    private static final String PATHKEY = "C://ИБ//ЛР1//КАЮ_key.key";
 
     private static void writeToFile(String path, byte[] buffer) {
         try (FileOutputStream fos = new FileOutputStream(path)) {
@@ -12,7 +10,22 @@ public class Main {
         }
     }
 
+    private static void printHelpToConsole() {
+        System.out.println("Вы должны ввести параметры в таком порядке:" + "\n"
+                + "Encrypt-1.jar param1 param2 param3" + "\n"
+                + "где param1 - полный путь к текстовому файлу для кодирования" + "\n"
+                + "пример: C:\\ИБ\\ЛР1\\1.txt" + "\n"
+                + "где param2 - полный путь к зашифрованному файлу" + "\n"
+                + "пример: C:\\ИБ\\ЛР1\\2.enc" + "\n"
+                + "где param3 - полный путь к сохранению файла ключа" + "\n"
+                + "пример: C:\\ИБ\\ЛР1\\КАЮ_Key.key" + "\n");
+    }
+
     public static void main(String[] args) throws IOException {
+        if (args[0].equals("?")) {
+            printHelpToConsole();
+            return;
+        }
         FileInputStream fin = new FileInputStream(args[0]);
         try {
             byte[] bufferValue = new byte[8];
@@ -21,7 +34,7 @@ public class Main {
             byte[] key = Tea.generateKey();
             int[] value = Transfer.byteToInt(bufferValue);
 
-            writeToFile(PATHKEY, key);
+            writeToFile(args[2], key);
             writeToFile(args[1], Tea.encrypt(value, Transfer.byteToInt(key)));
 
         } catch (IOException e) {
@@ -29,6 +42,5 @@ public class Main {
         } finally {
             fin.close();
         }
-
     }
 }
